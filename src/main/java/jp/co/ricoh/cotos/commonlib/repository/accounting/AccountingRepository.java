bc54cc6d82796ad64f81dcff58dd89cd099801f3
yamauchi <yasutaka.yamauchi@jrits.ricoh.co.jp>
2018-11-16 20:24:44 +0900

    Repository付与

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/AccountingRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/AccountingRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.accounting.Accounting;
 
+@Repository
 public interface AccountingRepository extends CrudRepository<Accounting, Long> {
 
 	public List<Accounting> findByCostTypeAndFfmFlag(String costType, int ffmFlag);
