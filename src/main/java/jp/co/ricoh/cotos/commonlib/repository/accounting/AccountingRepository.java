16d06c5d7ff9eaedd2a02108a5abaff51d4153e6
kakuhana <kentaroh.kakuhana@jrits.ricoh.co.jp>
2018-11-21 13:01:01 +0900

    メソッド追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/AccountingRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/AccountingRepository.java
@@ -25,4 +25,11 @@ public interface AccountingRepository extends CrudRepository<Accounting, Long> {
 			+ "and ac.ffm_flag = :ffmFlag", nativeQuery = true)
 	public int updateFfmFlagByCostTypeAndFfmFlag(@Param("costType") String costType, @Param("ffmFlag") int ffmFlag);
 
+	@Query(value = "select * from accounting ac "//
+			+ "where ac.costType = :costType and ac.ffmFlag = :ffmFlag "//
+			+ "and ac.product_type_cd in (select ricoh_item_code from item_master where product_master_id in "//
+			+ "(select pm.id from product_master pm left join product_comp_master pcm on pm.id = pcm.product_master_id "//
+			+ "where product_grp_master_id = :productGrpMasterId))", nativeQuery = true)
+	public List<Accounting> findByProductGrpMasterIdAndCostTypeAndFfmFlag(@Param("productGrpMasterId") String productGrpMasterId, @Param("costType") String costType, @Param("ffmFlag") int ffmFlag);
+
 }
