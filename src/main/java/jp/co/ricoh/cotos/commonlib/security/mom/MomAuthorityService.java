adaf65f636ecbb339856bb2ebfb8043027d9dace
kamada <tatsuya.kamada@jrits.ricoh.co.jp>
2019-10-01 13:59:33 +0900

    電力用権限情報(2230)データなし対応

--- src/main/java/jp/co/ricoh/cotos/commonlib/security/mom/MomAuthorityService.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/security/mom/MomAuthorityService.java
@@ -142,8 +142,12 @@ public class MomAuthorityService {
 			Map<AuthDiv, AuthLevel> authorities = Arrays.asList(AuthDiv.values()).stream().filter(authDiv -> authDiv != AuthDiv.なし).collect(Collectors.toMap(authDiv -> authDiv, authDiv -> {
 
 				// 該当権限区分のMoM権限レベルを取得
-				String targetAuthLevel = authorityInfoLevelDtoList.stream().filter(authorityInfoLevelDto -> authDiv.toString().equals(authorityInfoLevelDto.getInfoId())).findFirst().get().getLevelId();
-				return Arrays.asList(AuthLevel.values()).stream().filter(authLevel -> authLevel.value.equals(targetAuthLevel)).findFirst().get();
+				if (authorityInfoLevelDtoList.stream().filter(authorityInfoLevelDto -> authDiv.toString().equals(authorityInfoLevelDto.getInfoId())).count() > 0) {
+					String targetAuthLevel = authorityInfoLevelDtoList.stream().filter(authorityInfoLevelDto -> authDiv.toString().equals(authorityInfoLevelDto.getInfoId())).findFirst().get().getLevelId();
+					return Arrays.asList(AuthLevel.values()).stream().filter(authLevel -> authLevel.value.equals(targetAuthLevel)).findFirst().get();
+				} else {
+					return AuthLevel.不可;
+				}
 			}));
 
 			allMomAuthorities.put(actionDiv, authorities);
