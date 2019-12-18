b556bd4791b1dc9c0501ceeda8a1862f881d2808
宇田川慎司 <shinji.udagawa.us@jp.ricoh.com>
2019-12-18 21:56:57 +0900

    手配業務添付ファイル連携先リポジトリ修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/arrangement/ArrangementWorkAttachedFileLinkageRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/arrangement/ArrangementWorkAttachedFileLinkageRepository.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.commonlib.repository.arrangement;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import jp.co.ricoh.cotos.commonlib.entity.arrangement.ArrangementWorkAttachedFil
 
 @Repository
 public interface ArrangementWorkAttachedFileLinkageRepository extends CrudRepository<ArrangementWorkAttachedFileLinkage, Long> {
-
+	List<ArrangementWorkAttachedFileLinkage> findByArrangementWorkAttachedFileId(long arrangementWorkAttachedFileId);
 }
