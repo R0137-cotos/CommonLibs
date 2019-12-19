26e02f058ac1b7360150c9389eb9bba2c36c80db
Kanda Masashi <mkanda000700@rfg.rfgad.ricoh.com>
2019-12-19 15:33:22 +0900

    使用量レポジトリメソッド追加

--- src/test/java/jp/co/ricoh/cotos/commonlib/repository/TestAccounting.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/repository/TestAccounting.java
@@ -234,6 +234,11 @@ public class TestAccounting {
 
 		// Entity が null または 空 ではないことを確認
 		Assert.assertTrue(!CollectionUtils.isEmpty(founds));
+
+		found = usageQuantityRepository.findByRjManageNumberAndItemCode("rj_manage_number", "item_code");
+
+		// Entity が null ではないことを確認
+		Assert.assertNotNull(found);
 	}
 
 	@Test
