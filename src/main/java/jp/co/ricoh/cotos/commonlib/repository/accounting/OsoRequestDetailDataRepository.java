f0f3a49e074ea0733d8d28e28c0bca3409a10e2d
宇田川慎司 <shinji.udagawa.us@jp.ricoh.com>
2020-01-20 14:00:27 +0900

    OSO申込明細データリポジトリメソッド追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/OsoRequestDetailDataRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/accounting/OsoRequestDetailDataRepository.java
@@ -1,11 +1,15 @@
 package jp.co.ricoh.cotos.commonlib.repository.accounting;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import jp.co.ricoh.cotos.commonlib.entity.EnumType.OsoProcessingStatus;
 import jp.co.ricoh.cotos.commonlib.entity.accounting.OsoRequestDetailData;
+import jp.co.ricoh.cotos.commonlib.entity.common.OsoRequestDetailDataAbstractEntity.ProcessingDiv;
 
 @Repository
 public interface OsoRequestDetailDataRepository extends CrudRepository<OsoRequestDetailData, Long> {
-
+	public List<OsoRequestDetailData> findByProcessingDivAndOsoManageNumberAndItemCodeAndProcessingStatus(ProcessingDiv processingDiv, String osoManageNumber, String itemCode, OsoProcessingStatus processingStatus);
 }
