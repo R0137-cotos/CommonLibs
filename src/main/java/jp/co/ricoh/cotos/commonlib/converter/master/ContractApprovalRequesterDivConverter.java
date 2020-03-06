20d772a9e77c584848b68284c43b4c91a86c37f1
Yoshiki.Koiwa <Yoshiki.Koiwa@jp.ricoh.com>
2020-03-06 15:05:38 +0900

    コンフリクト解消

--- src/main/java/jp/co/ricoh/cotos/commonlib/converter/master/ContractApprovalRequesterDivConverter.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/converter/master/ContractApprovalRequesterDivConverter.java
@@ -1,9 +1,11 @@
 package jp.co.ricoh.cotos.commonlib.converter.master;
 
 import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 
 import jp.co.ricoh.cotos.commonlib.entity.master.ApprovalRouteMaster.ContractApprovalRequesterDiv;
 
+@Converter(autoApply = true)
 public class ContractApprovalRequesterDivConverter implements AttributeConverter<ContractApprovalRequesterDiv, String> {
 
 	@Override
