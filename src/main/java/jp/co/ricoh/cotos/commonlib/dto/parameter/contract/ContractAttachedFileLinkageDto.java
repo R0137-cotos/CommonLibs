dfeeb8903bde7eb2023be825d4462d2b1dea4140
tkamada <tatsuya.kamada@jrits.ricoh.co.jp>
2020-05-01 10:33:36 +0900

    契約添付ファイル連携先項目追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/contract/ContractAttachedFileLinkageDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/contract/ContractAttachedFileLinkageDto.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.commonlib.dto.parameter.contract;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -33,4 +35,12 @@ public class ContractAttachedFileLinkageDto extends DtoBase {
 	@NotNull
 	@ApiModelProperty(value = "連携ステータス", required = true, allowableValues = "連携対象外(\"0\"), 未連携(\"1\"), 連携済(\"2\")", example = "0", position = 5)
 	private FileLinkageStatus linkageStatus;
+	
+	/**
+	 * 解約フラグ
+	 */
+	@Max(9)
+	@Min(0)
+	@ApiModelProperty(value = "解約フラグ", required = false, position = 6, allowableValues = "range[0,9]")
+	private Integer disengagementFlg;
 }
