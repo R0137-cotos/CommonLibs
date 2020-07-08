e3d31f488e9780c0989db1bc7bda7ad323af5e3b
Yoshiki.Koiwa <Yoshiki.Koiwa@jp.ricoh.com>
2020-07-08 15:16:01 +0900

    #7515 契約情報確認、手配契約情報更新用のパラメータ―作成

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/arrangement/UpdateArrangementContractParameter.java
@@ -0,0 +1,22 @@
+package jp.co.ricoh.cotos.commonlib.dto.parameter.arrangement;
+
+import javax.validation.Valid;
+
+import io.swagger.annotations.ApiModelProperty;
+import jp.co.ricoh.cotos.commonlib.dto.parameter.contract.ContractDto;
+import lombok.Data;
+
+/**
+ * 手配契約情報更新のためのパラメータクラスを表します。
+ */
+@Data
+public class UpdateArrangementContractParameter {
+
+	@Valid
+	@ApiModelProperty(value = "手配", required = true)
+	private ArrangementDto arrangementDto;
+
+	@Valid
+	@ApiModelProperty(value = "契約", required = true)
+	private ContractDto contractDto;
+}
