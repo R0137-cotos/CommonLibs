ec77760eddb3810cf147b3d41cc0a35351681889
mkanda <masashi.kanda@jp.ricoh.com>
2020-09-01 14:51:17 +0900

    ContractOperationLog修正

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/ApplicationContextProvider.java
@@ -0,0 +1,37 @@
+package jp.co.ricoh.cotos.commonlib;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ *
+ * ApplicationContextのインスタンスを生成、返却する
+ *
+ */
+public class ApplicationContextProvider {
+
+	private static ApplicationContext context;
+
+	private ApplicationContextProvider() {
+	}
+
+	/**
+	 * ApplicationContextを取得
+	 * @return ApplicationContext
+	 */
+	public static ApplicationContext getApplicationContext() {
+		if (context == null) {
+			context = createApplicationContext();
+		}
+		return context;
+	}
+
+	/**
+	 * ApplicationContextを生成
+	 * @return ApplicationContext
+	 */
+	private static ApplicationContext createApplicationContext() {
+
+		ApplicationContext context = new AppricationContextGenerator("bean.xml");
+		return context;
+	}
+}
