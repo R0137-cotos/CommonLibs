459ac3b85c43485c5ef69107fad2319019cbc412
motomiya <Kana.Motomiya@jp.ricoh.com>
2020-09-02 13:17:30 +0900

    ContractOperationLogとContractListener修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/contract/ContractOperationLogListener.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/contract/ContractOperationLogListener.java
@@ -6,7 +6,6 @@ import javax.persistence.PrePersist;
 import javax.transaction.Transactional;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -17,6 +16,7 @@ import jp.co.ricoh.cotos.commonlib.entity.master.MvEmployeeMaster;
 import jp.co.ricoh.cotos.commonlib.exception.ErrorCheckException;
 import jp.co.ricoh.cotos.commonlib.exception.ErrorInfo;
 import jp.co.ricoh.cotos.commonlib.logic.check.CheckUtil;
+import jp.co.ricoh.cotos.commonlib.provider.UtilProvider;
 import jp.co.ricoh.cotos.commonlib.repository.master.DummyUserMasterRepository;
 import jp.co.ricoh.cotos.commonlib.repository.master.MvEmployeeMasterRepository;
 import jp.co.ricoh.cotos.commonlib.security.CotosAuthenticationDetails;
@@ -28,21 +28,6 @@ public class ContractOperationLogListener {
 	private static CheckUtil checkUtil;
 	private static DummyUserMasterRepository dummyUserMasterRepository;
 
-	@Autowired
-	public void setMvEmployeeMasterRepository(MvEmployeeMasterRepository mvEmployeeMasterRepository) {
-		ContractOperationLogListener.mvEmployeeMasterRepository = mvEmployeeMasterRepository;
-	}
-
-	@Autowired
-	public void setCheckUtil(CheckUtil checkUtil) {
-		ContractOperationLogListener.checkUtil = checkUtil;
-	}
-
-	//	@Autowired
-	//	public void setDummyUserMasterRepository(DummyUserMasterRepository dummyUserMasterRepository) {
-	//		ContractOperationLogListener.dummyUserMasterRepository = dummyUserMasterRepository;
-	//	}
-
 	/**
 	 * 社員マスタ情報を契約操作履歴トランザクションに紐づけます。
 	 * バッチユーザーの場合は固定値を設定します。
@@ -52,11 +37,13 @@ public class ContractOperationLogListener {
 	@PrePersist
 	@Transactional
 	public void appendsEmployeeFields(ContractOperationLog contractOperationLog) {
+		// Beanの取得
+		ApplicationContext context = ApplicationContextProvider.getApplicationContext();
+		mvEmployeeMasterRepository = context.getBean(MvEmployeeMasterRepository.class);
+		checkUtil = UtilProvider.getCheckUtil();
+		dummyUserMasterRepository = context.getBean(DummyUserMasterRepository.class);
 
 		CotosAuthenticationDetails userInfo = (CotosAuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-		ApplicationContext context = ApplicationContextProvider.getApplicationContext();
-		dummyUserMasterRepository = context.getBean(DummyUserMasterRepository.class);
 
 		if (userInfo.isDummyUser()) {
 			DummyUserMaster dummyUserMaster = dummyUserMasterRepository.findByUserId(userInfo.getMomEmployeeId());
