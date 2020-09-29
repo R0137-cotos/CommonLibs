0533d3d1c3119db012b6977badd14b5de6d10c55
motomiya <Kana.Motomiya@jp.ricoh.com>
2020-09-29 10:16:08 +0900

    cherry-pick1

---
+++ src/test/java/jp/co/ricoh/cotos/commonlib/check/TestSetMessageUtil.java
@@ -0,0 +1,55 @@
+package jp.co.ricoh.cotos.commonlib.check;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import jp.co.ricoh.cotos.commonlib.logic.check.CheckUtil;
+
+/**
+ * フォーマット変換確認メソッドのテストクラス
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TestSetMessageUtil {
+
+	@Value("${spring.messages.basename}")
+	String basename;
+	@Value("${spring.messages.defaultEncoding}")
+	String defaultEncoding;
+
+	static ConfigurableApplicationContext context;
+
+	@Autowired
+	public void injectContext(ConfigurableApplicationContext injectContext) {
+		context = injectContext;
+	}
+
+	@LocalServerPort
+	private int localServerPort;
+
+	@AfterClass
+	public static void stopAPServer() throws InterruptedException {
+		if (null != context) {
+			context.stop();
+		}
+	}
+
+	@Test
+	public void エラーが発生しないこと() {
+		try {
+			CheckUtil checkUtil = new CheckUtil();
+			checkUtil.setMessageUtil(basename, defaultEncoding);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+}
