abdd7e8162247ea337e4eb8a0857223ba5811f28
mkanda <masashi.kanda@jp.ricoh.com>
2020-10-07 16:49:34 +0900

    バージョン更新

--- src/main/java/jp/co/ricoh/cotos/commonlib/provider/AbstractXmlApplicationContextEx.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/provider/AbstractXmlApplicationContextEx.java
@@ -2,20 +2,30 @@ package jp.co.ricoh.cotos.commonlib.provider;
 
 import java.io.IOException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.ResourceEntityResolver;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 
+import jp.co.ricoh.cotos.commonlib.security.AccessLogOutputFilter;
+
 public abstract class AbstractXmlApplicationContextEx extends AbstractXmlApplicationContext {
+
+	private static final Log log = LogFactory.getLog(AccessLogOutputFilter.class);
 
 	public AbstractXmlApplicationContextEx(ApplicationContext parent) {
 		super(parent);
+
+		log.info("調査用：AbstractXmlApplicationContextEx constructor call");
 	}
 
 	@Override
 	protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) throws BeansException, IOException {
+		log.info("調査用：AbstractXmlApplicationContextEx.loadBeanDefinitions() start");
+
 		// Create a new XmlBeanDefinitionReader for the given BeanFactory.
 		XmlBeanDefinitionReaderEx beanDefinitionReader = new XmlBeanDefinitionReaderEx(beanFactory);
 
@@ -29,5 +39,7 @@ public abstract class AbstractXmlApplicationContextEx extends AbstractXmlApplica
 		// then proceed with actually loading the bean definitions.
 		initBeanDefinitionReader(beanDefinitionReader);
 		loadBeanDefinitions(beanDefinitionReader);
+
+		log.info("調査用：AbstractXmlApplicationContextEx.loadBeanDefinitions() end");
 	}
 }
--- src/main/java/jp/co/ricoh/cotos/commonlib/provider/ApplicationContextProvider.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/provider/ApplicationContextProvider.java
@@ -1,6 +1,10 @@
 package jp.co.ricoh.cotos.commonlib.provider;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
+
+import jp.co.ricoh.cotos.commonlib.security.AccessLogOutputFilter;
 
 /**
  *
@@ -8,6 +12,8 @@ import org.springframework.context.ApplicationContext;
  *
  */
 public class ApplicationContextProvider {
+
+	private static final Log log = LogFactory.getLog(AccessLogOutputFilter.class);
 
 	private static ApplicationContext context;
 
@@ -19,9 +25,13 @@ public class ApplicationContextProvider {
 	 * @return ApplicationContext
 	 */
 	public static ApplicationContext getApplicationContext() {
+		log.info("調査用：ApplicationContextProvider.getApplicationContext() start");
+
 		if (context == null) {
 			context = createApplicationContext();
 		}
+
+		log.info("調査用：ApplicationContextProvider.getApplicationContext() end");
 		return context;
 	}
 
@@ -30,8 +40,11 @@ public class ApplicationContextProvider {
 	 * @return ApplicationContext
 	 */
 	private static ApplicationContext createApplicationContext() {
+		log.info("調査用：ApplicationContextProvider.createApplicationContext() start");
 
 		ApplicationContext context = new AppricationContextGenerator("bean.xml");
+
+		log.info("調査用：ApplicationContextProvider.createApplicationContext() end");
 		return context;
 	}
 }
