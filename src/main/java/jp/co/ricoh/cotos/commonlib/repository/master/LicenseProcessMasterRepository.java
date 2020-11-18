7d3cf024f8fbd3a535b0eef9d8e28040dfd79772
Mitsunaga Norito <z00se05127@rfg.rfgad.ricoh.com>
2020-11-18 11:55:44 +0900

    #8689 ライセンス情報DTOとリポジトリ追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/LicenseProcessMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/LicenseProcessMasterRepository.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.commonlib.repository.master;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import jp.co.ricoh.cotos.commonlib.entity.master.LicenseProcessMaster;
 
 @Repository
 public interface LicenseProcessMasterRepository extends CrudRepository<LicenseProcessMaster, Long> {
+
+	public List<LicenseProcessMaster> findByidIn(List<Long> id);
 }
