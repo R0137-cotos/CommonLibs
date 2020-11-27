5b74ba8a658bdeef6c190c11999dafe7df6445f6
takumi-sato <sato-takumi@softcomm.co.jp>
2020-11-27 12:11:50 +0900

    検索用手配業務タイプマスタエンティティ・リポジトリ追加

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/ArrangementWorkTypeForSearchMaster.java
@@ -0,0 +1,48 @@
+package jp.co.ricoh.cotos.commonlib.entity.master;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModelProperty;
+import jp.co.ricoh.cotos.commonlib.entity.EntityBaseMaster;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 検索用手配業務タイプマスタを表すEntity
+ */
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "arrangement_work_type_for_search_master")
+public class ArrangementWorkTypeForSearchMaster extends EntityBaseMaster {
+
+	/**
+	 * 検索用手配業務タイプマスタID
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "arrangement_work_type_for_search_master_seq")
+	@SequenceGenerator(name = "arrangement_work_type_for_search_master_seq", sequenceName = "arrangement_work_type_for_search_master_seq", allocationSize = 1)
+	@ApiModelProperty(value = "検索用手配業務タイプマスタID", required = true, position = 1, allowableValues = "range[0,9999999999999999999]")
+	private long id;
+
+	/**
+	 * 商品マスタID
+	 */
+	@Column(nullable = false)
+	@ApiModelProperty(value = "商品マスタID", required = true, position = 2)
+	private long productMasterId;
+
+	/**
+	 * 手配業務タイプマスタID
+	 */
+	@Column(nullable = false)
+	@ApiModelProperty(value = "手配業務タイプマスタID", required = true, position = 3)
+	private long arrangementWorkTypeMasterId;
+
+}
