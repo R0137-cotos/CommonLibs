4976745b694785daa184cbeabcb188b1d7067e81
Michino Kanako <z00se05215@rfg.rfgad.ricoh.com>
2020-11-30 14:34:46 +0900

    #9403_リポジトリにライセンス区分マスタ一覧取得を追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/LicenseDivMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/LicenseDivMasterRepository.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.commonlib.repository.master;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import jp.co.ricoh.cotos.commonlib.entity.master.LicenseDivMaster;
 
 @Repository
 public interface LicenseDivMasterRepository extends CrudRepository<LicenseDivMaster, Long> {
-
+	public List<LicenseDivMaster> findAllByOrderByIdAsc ();
 }
