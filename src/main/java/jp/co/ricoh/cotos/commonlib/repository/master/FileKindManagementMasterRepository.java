dcbbec91416f6831ec173d73addeb89a330ad5a3
Michino Kanako <z00se05215@rfg.rfgad.ricoh.com>
2020-12-02 15:06:47 +0900

    #8716_リポジトリの修正(ファイル種別管理マスタ)

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/FileKindManagementMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/FileKindManagementMasterRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.master.FileKindManagementMaster;
-import jp.co.ricoh.cotos.commonlib.entity.master.FileKindManagementMaster.ImportExportType;
 import jp.co.ricoh.cotos.commonlib.entity.master.FileOperationRelationProductMaster;
 
 @Repository
 public interface FileKindManagementMasterRepository extends CrudRepository<FileKindManagementMaster, Long> {
-	public List<FileKindManagementMaster> findByFileOperationRelationProductMasterInAndImportExportTypeOrderByIdAsc(List<FileOperationRelationProductMaster> fileOperationRelationProductMasterList, ImportExportType importExportType);
+	public List<FileKindManagementMaster> findByFileOperationRelationProductMasterInOrderByIdAsc(List<FileOperationRelationProductMaster> fileOperationRelationProductMasterList);
 }
