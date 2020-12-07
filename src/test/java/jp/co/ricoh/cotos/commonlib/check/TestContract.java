7966c3a8a7173a5fe88c4b239249162f45de33e5
Furukawa Takashi <z00se05002@rfg.rfgad.ricoh.com>
2020-12-07 20:22:44 +0900

    #8544_テストコード修正

--- src/test/java/jp/co/ricoh/cotos/commonlib/check/TestContract.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/check/TestContract.java
@@ -715,7 +715,7 @@ public class TestContract {
 		testTarget.setPurchaseManageNumber(STR_256);
 		testTarget.setToVendorComment(STR_1334);
 		result = testSecurityController.callParameterCheck(testTarget, headersProperties, localServerPort);
-		Assert.assertTrue(result.getErrorInfoList().size() == 23);
+		Assert.assertTrue(result.getErrorInfoList().size() == 24);
 		Assert.assertTrue(testTool.errorIdMatchesAll(result.getErrorInfoList(), ParameterErrorIds.ROT00014));
 		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "変更元文書番号は最大文字数（255）を超えています。"));
 
