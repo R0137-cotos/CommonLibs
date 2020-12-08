2a11c5d27ebd8eef649e388feed76051ca1483c1
Mitsunaga Norito <z00se05127@rfg.rfgad.ricoh.com>
2020-12-08 11:11:58 +0900

    #8694 MailAddressUtil実装

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/MailInfoDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/MailInfoDto.java
@@ -3,6 +3,8 @@ package jp.co.ricoh.cotos.commonlib.dto.result;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -18,20 +20,28 @@ public class MailInfoDto {
 	private long mailTemplateMasterId;
 
 	/**
+	 * メールタイプ区分
+	 */
+	@NotNull
+	@Size(max = 255)
+	@ApiModelProperty(value = "メールテンプレートマスタID", required = true, position = 2, allowableValues = "range[0,255]")
+	private String mailTypeDiv;
+
+	/**
 	 * TOメールアドレスリスト
 	 */
-	@ApiModelProperty(value = "TOメールアドレスリスト", required = false, position = 2)
+	@ApiModelProperty(value = "TOメールアドレスリスト", required = false, position = 3)
 	private List<String> toMailAddressList;
 
 	/**
 	 * CCメールアドレスリスト
 	 */
-	@ApiModelProperty(value = "CCメールアドレスリスト", required = false, position = 3)
+	@ApiModelProperty(value = "CCメールアドレスリスト", required = false, position = 4)
 	private List<String> ccMailAddressList;
 
 	/**
-	 * bccメールアドレスリスト
+	 * BCCメールアドレスリスト
 	 */
-	@ApiModelProperty(value = "BCCメールアドレスリスト", required = false, position = 4)
+	@ApiModelProperty(value = "BCCメールアドレスリスト", required = false, position = 5)
 	private List<String> bccMailAddressList;
 }
