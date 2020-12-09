807a845ea260dfadc205816f5cb23e54d8c574ea
Yoshiki.Koiwa <Yoshiki.Koiwa@jp.ricoh.com>
2020-12-09 16:14:31 +0900

    #9441 軽量バッチでもメール送信機能を使えるように修正

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/provider/ApplicationContextProvider.java
@@ -0,0 +1,34 @@
+package jp.co.ricoh.cotos.commonlib.provider;
+
+import org.springframework.context.ApplicationContext;
+
+import jp.co.ricoh.cotos.commonlib.config.AppricationContextGenerator;
+
+public class ApplicationContextProvider {
+	private static ApplicationContext context;
+
+	private ApplicationContextProvider() {
+	}
+
+	/**
+	 * ApplicationContextを取得
+	 * @return ApplicationContext
+	 */
+	public static ApplicationContext getApplicationContext() {
+		if (context == null) {
+			context = createApplicationContext();
+		}
+		return context;
+	}
+
+	/**
+	 * ApplicationContextを生成
+	 * @return ApplicationContext
+	 */
+	private static ApplicationContext createApplicationContext() {
+
+		ApplicationContext context = new AppricationContextGenerator("bean.xml");
+		return context;
+	}
+
+}
