a6a4b30d62eacb84c112916d0ab43568ea2dd6e0
Michino Kanako <z00se05215@rfg.rfgad.ricoh.com>
2020-12-29 09:55:43 +0900

    #8584_DB修正に伴うCommonLibsの修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/MailControlMaster.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/MailControlMaster.java
@@ -14,7 +14,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -78,50 +77,6 @@ public class MailControlMaster extends EntityBaseMaster {
 
 		@JsonCreator
 		public static ContactReferenceType fromString(String string) {
-			return Arrays.stream(values()).filter(v -> v.text.equals(string)).findFirst().orElseThrow(() -> new IllegalArgumentException(String.valueOf(string)));
-		}
-	}
-
-	public enum NotificatnDataDifferenceType {
-
-		日換算("1"), 月換算("2");
-
-		private final String text;
-
-		private NotificatnDataDifferenceType(final String text) {
-			this.text = text;
-		}
-
-		@Override
-		@JsonValue
-		public String toString() {
-			return this.text;
-		}
-
-		@JsonCreator
-		public static NotificatnDataDifferenceType fromString(String string) {
-			return Arrays.stream(values()).filter(v -> v.text.equals(string)).findFirst().orElseThrow(() -> new IllegalArgumentException(String.valueOf(string)));
-		}
-	}
-
-	public enum BusinessDayType {
-
-		前営業日("1"), 後営業日("2");
-
-		private final String text;
-
-		private BusinessDayType(final String text) {
-			this.text = text;
-		}
-
-		@Override
-		@JsonValue
-		public String toString() {
-			return this.text;
-		}
-
-		@JsonCreator
-		public static BusinessDayType fromString(String string) {
 			return Arrays.stream(values()).filter(v -> v.text.equals(string)).findFirst().orElseThrow(() -> new IllegalArgumentException(String.valueOf(string)));
 		}
 	}
@@ -228,31 +183,4 @@ public class MailControlMaster extends EntityBaseMaster {
 	@OneToMany(mappedBy = "mailControlMaster")
 	@ApiModelProperty(value = "通知メール対象商材マスタ", required = false, position = 15)
 	private List<MailProductMaster> mailProductMasterList;
-
-	/**
-	 * 通知日差分区分
-	 */
-	@ApiModelProperty(value = "通知日差分区分", required = false, position = 16, allowableValues = "日換算(\"1\"), 月換算(\"2\")")
-	private NotificatnDataDifferenceType notificatnDataDifferenceType;
-
-	/**
-	 * 通知日計算日数
-	 */
-	@Size(max = 255)
-	@ApiModelProperty(value = "通知日計算日数", required = false, position = 17, allowableValues = "range[0,255]")
-	private String notificatnDataAddDay;
-
-	/**
-	 * 営業日フラグ
-	 */
-	@Max(9)
-	@Min(0)
-	@ApiModelProperty(value = "営業日フラグ", required = false, position = 18, allowableValues = "range[0,9]")
-	private Integer businessDayFlg;
-
-	/**
-	 * 営業日計算区分
-	 */
-	@ApiModelProperty(value = "営業日計算区分", required = false, position = 19, allowableValues = "前営業日(\"1\"), 後営業日(\"2\")")
-	private BusinessDayType businessDayType;
 }
