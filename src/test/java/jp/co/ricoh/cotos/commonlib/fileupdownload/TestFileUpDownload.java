b96c10331ab78823ead01c8d4df5eb7d84099971
Furukawa Takashi W (古川 宇宙) <takashi.furukawa.W@jp.ricoh.com>
2021-01-18 17:11:55 +0900

    #8710_ZIP化メソッドを追加

--- src/test/java/jp/co/ricoh/cotos/commonlib/fileupdownload/TestFileUpDownload.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/fileupdownload/TestFileUpDownload.java
@@ -1,9 +1,13 @@
 package jp.co.ricoh.cotos.commonlib.fileupdownload;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,13 +24,18 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StreamUtils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jp.co.ricoh.cotos.commonlib.DBConfig;
 import jp.co.ricoh.cotos.commonlib.WithMockCustomUser;
+import jp.co.ricoh.cotos.commonlib.dto.parameter.common.CreateZipParameter;
 import jp.co.ricoh.cotos.commonlib.entity.common.AttachedFile;
 import jp.co.ricoh.cotos.commonlib.exception.ErrorCheckException;
+import jp.co.ricoh.cotos.commonlib.exception.ErrorInfo;
 import jp.co.ricoh.cotos.commonlib.logic.fileupdownload.FileUpDownload;
 import jp.co.ricoh.cotos.commonlib.repository.common.AttachedFileRepository;
 import jp.co.ricoh.cotos.commonlib.util.AppProperties;
+import net.lingala.zip4j.exception.ZipException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -42,6 +51,9 @@ public class TestFileUpDownload {
 	AppProperties appProperties;
 
 	static ConfigurableApplicationContext context;
+
+	@Autowired
+	ObjectMapper mapper;
 
 	@Autowired
 	public void injectContext(ConfigurableApplicationContext injectContext) {
@@ -156,7 +168,7 @@ public class TestFileUpDownload {
 			Assert.assertEquals("エラーメッセージが正しく設定されること", "指定されたファイルが存在しません。", e.getErrorInfoList().get(0).getErrorMessage());
 		}
 	}
-	
+
 	@Test
 	@Ignore
 	public void EIMファイルダウンロード() throws Exception {
@@ -168,9 +180,211 @@ public class TestFileUpDownload {
 		}
 	}
 
+	@Test
+	public void ZIP圧縮_暗号化なし_削除あり_ディレクトリとファイル() {
+		アップロードディレクトリファイル削除();
+		String path = appProperties.getFileProperties().getUploadFileDir();
+		// 圧縮対象の設定
+		try {
+			ファイルコピー("/src/test/resources/attachmentFiles/testFile2.txt", "testFile2_999.txt");
+			new File(path + "/test_attachmentFiles").mkdir();
+			ファイルコピー("/src/test/resources/attachmentFiles/testFile2.txt", "test_attachmentFiles/testFile2_999.txt");
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+
+		CreateZipParameter zipParam = new CreateZipParameter();
+		List<String> inputPathList = new ArrayList<>();
+		inputPathList.add(path + "/testFile2_999.txt");
+		inputPathList.add(path + "/test_attachmentFiles");
+		zipParam.setInputPathList(inputPathList);
+		zipParam.setOutputPath(appProperties.getFileProperties().getUploadFileDir() + "/暗号化なし.zip");
+		zipParam.setFileNameCharset("MS932");
+		zipParam.setPassword("");
+		zipParam.setInputFileDeleteFlg(true);
+		try {
+			byte[] zipBytes = fileUpDownload.createZipAndDelete(zipParam);
+
+			File zip = new File(zipParam.getOutputPath());
+			Assert.assertFalse("作成したzipファイルが削除されていること", zip.exists());
+
+			zipParam.getInputPathList().stream().forEach(input -> Assert.assertFalse("zipファイルのソースが削除されていること", new File(input).exists()));
+
+			// zipファイルを作成する
+			Files.write(zip.toPath(), zipBytes);
+			Assert.assertTrue("zipファイルが存在すること", zip.exists());
+			Assert.assertEquals("ファイル名が想定通りであること（文字化けしていないこと）", "暗号化なし.zip", zip.getName());
+
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		} finally {
+			アップロードディレクトリファイル削除();
+			new File(zipParam.getOutputPath()).delete();
+		}
+	}
+
+	@Test
+	public void ZIP圧縮_暗号化あり_削除なし_ファイルのみ() {
+		アップロードディレクトリファイル削除();
+
+		// 圧縮対象の設定
+		try {
+			ファイルコピー("/src/test/resources/attachmentFiles/testFile2.txt", "testFile2_999.txt");
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+
+		String path = appProperties.getFileProperties().getUploadFileDir();
+		CreateZipParameter zipParam = new CreateZipParameter();
+		List<String> inputPathList = new ArrayList<>();
+		inputPathList.add(path + "/testFile2_999.txt");
+		zipParam.setInputPathList(inputPathList);
+		zipParam.setOutputPath(appProperties.getFileProperties().getUploadFileDir() + "/暗号化あり.zip");
+		zipParam.setFileNameCharset("MS932");
+		zipParam.setPassword("test");
+		zipParam.setInputFileDeleteFlg(false);
+		try {
+			byte[] zipBytes = fileUpDownload.createZipAndDelete(zipParam);
+
+			File zip = new File(zipParam.getOutputPath());
+			Assert.assertFalse("作成したzipファイルが削除されていること", zip.exists());
+
+			zipParam.getInputPathList().stream().forEach(input -> Assert.assertTrue("zipファイルのソースが削除されていないこと", new File(input).exists()));
+
+			// zipファイルを作成する
+			Files.write(zip.toPath(), zipBytes);
+			Assert.assertTrue("zipファイルが存在すること", zip.exists());
+			Assert.assertEquals("ファイル名が想定通りであること（文字化けしていないこと）", "暗号化あり.zip", zip.getName());
+
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		} finally {
+			アップロードディレクトリファイル削除();
+			new File(zipParam.getOutputPath()).delete();
+		}
+	}
+
+	@Test
+	public void ZIP圧縮_異常系_パラメータ自体がnull() {
+		try {
+			fileUpDownload.createZipAndDelete(null);
+			fail("異常系のテストで正常終了した");
+		} catch (ErrorCheckException e) {
+			List<ErrorInfo> errorList = e.getErrorInfoList();
+			// チェック
+			assertEquals("エラー件数が一致すること", 1, errorList.size());
+			assertEquals("エラーIDが一致すること", "ROT00013", e.getErrorInfoList().get(0).getErrorId());
+			assertEquals("エラーメッセージが一致すること", "Zipファイル作成パラメータが設定されていません。", e.getErrorInfoList().get(0).getErrorMessage());
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+	}
+
+	@Test
+	public void ZIP圧縮_異常系_パラメータ内部チェック() {
+		CreateZipParameter zipParam = new CreateZipParameter();
+		try {
+			fileUpDownload.createZipAndDelete(zipParam);
+			fail("異常系のテストで正常終了した");
+		} catch (ErrorCheckException e) {
+			List<ErrorInfo> errorList = e.getErrorInfoList();
+			// チェック
+			assertEquals("エラー件数が一致すること", 1, errorList.size());
+			assertEquals("エラーIDが一致すること", "ROT00001", e.getErrorInfoList().get(0).getErrorId());
+			assertEquals("エラーメッセージが一致すること", "パラメータ「圧縮対象ファイル/ディレクトリ」が設定されていません。", e.getErrorInfoList().get(0).getErrorMessage());
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+
+		List<String> inputList = new ArrayList<>();
+		inputList.add("AAAAA");
+		zipParam.setInputPathList(inputList);
+		try {
+			fileUpDownload.createZipAndDelete(zipParam);
+			fail("異常系のテストで正常終了した");
+		} catch (ErrorCheckException e) {
+			List<ErrorInfo> errorList = e.getErrorInfoList();
+			// チェック
+			assertEquals("エラー件数が一致すること", 1, errorList.size());
+			assertEquals("エラーIDが一致すること", "ROT00001", e.getErrorInfoList().get(0).getErrorId());
+			assertEquals("エラーメッセージが一致すること", "パラメータ「圧縮先ファイル」が設定されていません。", e.getErrorInfoList().get(0).getErrorMessage());
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+
+		zipParam.setOutputPath("AAAAA/BBB");
+		try {
+			fileUpDownload.createZipAndDelete(zipParam);
+			fail("異常系のテストで正常終了した");
+		} catch (ErrorCheckException e) {
+			List<ErrorInfo> errorList = e.getErrorInfoList();
+			// チェック
+			assertEquals("エラー件数が一致すること", 1, errorList.size());
+			assertEquals("エラーIDが一致すること", "ROT00001", e.getErrorInfoList().get(0).getErrorId());
+			assertEquals("エラーメッセージが一致すること", "パラメータ「パスワード」が設定されていません。", e.getErrorInfoList().get(0).getErrorMessage());
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+
+		zipParam.setPassword("");
+		try {
+			fileUpDownload.createZipAndDelete(zipParam);
+			fail("異常系のテストで正常終了した");
+		} catch (ErrorCheckException e) {
+			List<ErrorInfo> errorList = e.getErrorInfoList();
+			// チェック
+			assertEquals("エラー件数が一致すること", 1, errorList.size());
+			assertEquals("エラーIDが一致すること", "ROT00001", e.getErrorInfoList().get(0).getErrorId());
+			assertEquals("エラーメッセージが一致すること", "パラメータ「ファイル名文字コード」が設定されていません。", e.getErrorInfoList().get(0).getErrorMessage());
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+
+		zipParam.setFileNameCharset("");
+		try {
+			fileUpDownload.createZipAndDelete(zipParam);
+			fail("異常系のテストで正常終了した");
+		} catch (ErrorCheckException e) {
+			List<ErrorInfo> errorList = e.getErrorInfoList();
+			// チェック
+			assertEquals("エラー件数が一致すること", 1, errorList.size());
+			assertEquals("エラーIDが一致すること", "ROT00003", e.getErrorInfoList().get(0).getErrorId());
+			assertEquals("エラーメッセージが一致すること", "圧縮対象ファイル/ディレクトリが特定できません。", e.getErrorInfoList().get(0).getErrorMessage());
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+
+		inputList = new ArrayList<>();
+		inputList.add(appProperties.getFileProperties().getUploadFileDir());
+		zipParam.setInputPathList(inputList);
+		try {
+			fileUpDownload.createZipAndDelete(zipParam);
+			fail("異常系のテストで正常終了した");
+		} catch (ErrorCheckException e) {
+			List<ErrorInfo> errorList = e.getErrorInfoList();
+			// チェック
+			assertEquals("エラー件数が一致すること", 1, errorList.size());
+			assertEquals("エラーIDが一致すること", "ROT00003", e.getErrorInfoList().get(0).getErrorId());
+			assertEquals("エラーメッセージが一致すること", "圧縮先ディレクトリが特定できません。", e.getErrorInfoList().get(0).getErrorMessage());
+		} catch (ZipException | IOException e) {
+			e.printStackTrace();
+			Assert.fail("想定外のエラーが発生");
+		}
+	}
+
 	/**
 	 * ファイル情報作成
-	 * 
+	 *
 	 * @param fileNm
 	 *            ファイル名
 	 * @return ファイル情報
@@ -185,7 +399,7 @@ public class TestFileUpDownload {
 
 	/**
 	 * ファイル情報作成(IE)
-	 * 
+	 *
 	 * @param fileNm
 	 *            ファイル名
 	 * @return ファイル情報
@@ -220,7 +434,7 @@ public class TestFileUpDownload {
 
 	/**
 	 * ファイルコピー
-	 * 
+	 *
 	 * @param copyFileNm
 	 *            コピー元ファイル
 	 * @param fileNm
@@ -251,7 +465,7 @@ public class TestFileUpDownload {
 
 	/**
 	 * ファイル拡張子取得
-	 * 
+	 *
 	 * @param fileName
 	 *            ファイル名
 	 * @return 拡張子
