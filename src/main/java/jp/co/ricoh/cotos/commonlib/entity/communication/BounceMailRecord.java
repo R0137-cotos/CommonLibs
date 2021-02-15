cc5bb8b83a88d8b35d9668ee4027e25a5c9681c2
Tamura Daiju (田村 大樹) <daiju.tamura@jp.ricoh.com>
2021-02-15 14:58:14 +0900

    バウンスメール記録にCOTOS運用対応完了済フラグを追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/communication/BounceMailRecord.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/communication/BounceMailRecord.java
@@ -154,5 +154,11 @@ public class BounceMailRecord extends EntityBase {
 	@OneToMany(mappedBy = "bounceMailRecord")
 	@ApiModelProperty(value = "バウンスメール宛先", required = false, position = 16)
 	private List<BounceMailDestination> bounceMailDestinationList;
+	
+	/**
+	 * COTOS運用対応完了済フラグ
+	 */
+	@ApiModelProperty(value = "COTOS運用対応完了済フラグ", required = true, position = 17, allowableValues = "false:COTOS運用チームに残作業がある true:COTOS運用チームに残作業がない。")
+	private boolean cotosSupportCompleted;
 
 }
