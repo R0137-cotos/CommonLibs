f120ea98becd86ed43178b9a17946d4b64155804
tkamada <tatsuya.kamada@jrits.ricoh.co.jp>
2021-04-01 13:46:58 +0900

    見積、契約手配業務添付ファイルリポジトリ修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/estimation/EstimationAttachedFileRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/estimation/EstimationAttachedFileRepository.java
@@ -2,7 +2,10 @@ package jp.co.ricoh.cotos.commonlib.repository.estimation;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.estimation.EstimationAttachedFile;
@@ -10,4 +13,8 @@ import jp.co.ricoh.cotos.commonlib.entity.estimation.EstimationAttachedFile;
 @Repository
 public interface EstimationAttachedFileRepository extends CrudRepository<EstimationAttachedFile, Long> {
 	public List<EstimationAttachedFile> findByEstimationId(Long estimationId);
+
+	@Modifying
+	@Query(value = "update estimation_attached_file set estimation_id = :estimationId, updated_at = sysdate where id = :id", nativeQuery = true)
+	public Integer updateEstimationAttachedFile(@Param("id") Long id, @Param("estimationId") Long estimationId);
 }
