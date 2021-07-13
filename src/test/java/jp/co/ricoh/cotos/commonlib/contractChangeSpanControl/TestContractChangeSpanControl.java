da3ce6c062b87cc907a738a2262414e43f8ad03a
Nabeta Kyohei (鍋多 恭平) <Kyohei.Nabeta@jp.ricoh.com>
2021-07-13 14:17:08 +0900

    #11145_テスト修正

--- src/test/java/jp/co/ricoh/cotos/commonlib/contractChangeSpanControl/TestContractChangeSpanControl.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/contractChangeSpanControl/TestContractChangeSpanControl.java
@@ -454,7 +454,7 @@ public class TestContractChangeSpanControl {
 		Assert.assertEquals("エラーリストの件数が１件であること", 1, errors.size());
 		errors.stream().forEach(err -> {
 			Assert.assertEquals("エラーIDが正しく設定されること", "RCO00045", err.getErrorId());
-			Assert.assertEquals("契約更新可能期間外のため契約の承認依頼は行えません。（契約更新可能期間：aから、bまで）",  err.getErrorMessage());
+			Assert.assertEquals("契約更新可能期間外のため契約の承認依頼・承認は行えません。（契約更新可能期間：aから、bまで）", err.getErrorMessage());
 		});
 	}
 
