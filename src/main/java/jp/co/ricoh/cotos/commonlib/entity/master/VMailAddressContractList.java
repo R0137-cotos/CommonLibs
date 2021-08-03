351457ba2637e3bc0857d1aea86e859cb7fa7557
tkamada <tatsuya.kamada@jrits.ricoh.co.jp>
2021-08-03 14:54:40 +0900

    メールパフォーマンス問題対応

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/VMailAddressContractList.java
@@ -0,0 +1,11 @@
+package jp.co.ricoh.cotos.commonlib.entity.master;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import jp.co.ricoh.cotos.commonlib.entity.common.VMailAddressListAbstractEntity;
+
+@Entity
+@Table(name = "v_mail_address_contract_list")
+public class VMailAddressContractList extends VMailAddressListAbstractEntity {
+}
