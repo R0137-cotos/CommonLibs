a1af39769c29a3c664a9a0dacc5da5f80ee6207c
Nabeta Kyohei (鍋多 恭平) <Kyohei.Nabeta@jp.ricoh.com>
2021-08-18 14:06:32 +0900

    #11345_契約、手配エンティティに契約状態遷移制御区分を追加

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/converter/contract/ContractStatusControlTypeConverter.java
@@ -0,0 +1,23 @@
+package jp.co.ricoh.cotos.commonlib.converter.contract;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import jp.co.ricoh.cotos.commonlib.entity.contract.Contract.ContractStatusControlType;
+
+@Converter(autoApply = true)
+public class ContractStatusControlTypeConverter implements AttributeConverter<ContractStatusControlType, String> {
+	@Override
+	public String convertToDatabaseColumn(ContractStatusControlType contractStatusControlType) {
+		if (contractStatusControlType == null)
+			return null;
+		return contractStatusControlType.toString();
+	}
+
+	@Override
+	public ContractStatusControlType convertToEntityAttribute(String value) {
+		if (value == null)
+			return null;
+		return ContractStatusControlType.fromString(value); // IllegalArgumentExceptionはContractType.fromString側で投げている
+	}
+}
