cff6e8a45b1b0b32d05759d65361728a715aaada
tkamada <tatsuya.kamada@jrits.ricoh.co.jp>
2021-09-21 11:05:46 +0900

    追加修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/logic/businessday/BusinessDayUtil.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/logic/businessday/BusinessDayUtil.java
@@ -105,7 +105,7 @@ public class BusinessDayUtil {
 			}
 		}
 
-		return DateUtils.truncate(retDate, Calendar.DAY_OF_MONTH);
+		return truncateDate(retDate);
 	}
 
 	/**
@@ -130,7 +130,7 @@ public class BusinessDayUtil {
 			}
 		}
 
-		return DateUtils.truncate(retDate, Calendar.DAY_OF_MONTH);
+		return truncateDate(retDate);
 	}
 
 	/**
@@ -159,7 +159,7 @@ public class BusinessDayUtil {
 			}
 		}
 
-		return DateUtils.truncate(retDate, Calendar.DAY_OF_MONTH);
+		return truncateDate(retDate);
 	}
 
 	/**
