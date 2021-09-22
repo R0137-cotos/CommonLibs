b49a0e77bead87617dbe429a557bb40f4327c86f
abe <Yoshiki.Abe1@jp.ricoh.com>
2021-09-22 13:24:42 +0900

    #12267 RSI契約管理APIのURLをプロパティに追加&エラーメッセージ追加

---
+++ src/test/java/jp/co/ricoh/cotos/commonlib/util/TestRsiPropertie.java
@@ -0,0 +1,42 @@
+package jp.co.ricoh.cotos.commonlib.util;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import jp.co.ricoh.cotos.commonlib.DBConfig;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TestRsiPropertie {
+
+	static ConfigurableApplicationContext context;
+
+	@Autowired
+	RsiProperties rsiProperties;
+
+	@Autowired
+	public void injectContext(ConfigurableApplicationContext injectContext) {
+		context = injectContext;
+		context.getBean(DBConfig.class).clearData();
+	}
+
+	@AfterClass
+	public static void stopAPServer() throws InterruptedException {
+		if (null != context) {
+			context.getBean(DBConfig.class).clearData();
+			context.stop();
+		}
+	}
+
+	@Test
+	public void RSIのプロパティが取得できること() {
+		Assert.assertEquals("RSI 契約管理APIのベースURLが取得できること", rsiProperties.getContractUrl(), "dummy_rsi_contract_url");
+	}
+}
