f3e2cf885f1e22ea6ddeadf75ca6502b24ec0f25
Nabeta Kyohei (鍋多 恭平) <Kyohei.Nabeta@jp.ricoh.com>
2021-10-12 17:15:42 +0900

    #11117_バウンスメール記録リポジトリに検索メソッドを追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/communication/BounceMailRecordRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/communication/BounceMailRecordRepository.java
@@ -3,7 +3,9 @@ package jp.co.ricoh.cotos.commonlib.repository.communication;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.EnumType.ServiceCategory;
@@ -16,4 +18,7 @@ public interface BounceMailRecordRepository extends CrudRepository<BounceMailRec
 	public List<BounceMailRecord> findByDocNumberAndContractBranchNumber(String docNumber, Integer contractBranchNumber);
 
 	public List<BounceMailRecord> findByCotosSupportCompletedAndMailControlMasterIdAndServiceCategoryAndTargetDocKeyOrderById(Integer cotosSupportCompleted, Long mailControlMasterId, ServiceCategory serviceCategory, String targetDocKey);
+
+	@Query(value = "SELECT * FROM BOUNCE_MAIL_RECORD WHERE COTOS_SUPPORT_COMPLETED IS NULL AND MAIL_CONTROL_MASTER_ID = :MAIL_CONTROL_MASTER_ID AND SERVICE_CATEGORY = :SERVICE_CATEGORY AND TARGET_DOC_KEY = :TARGET_DOC_KEY", nativeQuery = true)
+	public List<BounceMailRecord> findByMailControlMasterIdAndServiceCategoryAndTargetDocKeyOrderById(@Param("MAIL_CONTROL_MASTER_ID") Long mailControlMasterId, @Param("SERVICE_CATEGORY") ServiceCategory serviceCategory, @Param("TARGET_DOC_KEY") String targetDocKey);
 }
