71fbeb56bab4b9328a67951e8c009324d1b03afb
Koiwa Yoshiki (小岩 祥希) <Yoshiki.Koiwa@jp.ricoh.com>
2021-10-19 13:15:18 +0900

    @Temporalのタイプ修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/ContractListInfo.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/ContractListInfo.java
@@ -122,14 +122,14 @@ public class ContractListInfo {
 	/**
 	 * サービス開始日
 	 */
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
 	@ApiModelProperty(value = "サービス開始日", required = false, position = 16)
 	private Date serviceTermStart;
 
 	/**
 	 * サービス終了日
 	 */
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
 	@ApiModelProperty(value = "サービス終了日", required = false, position = 17)
 	private Date serviceTermEnd;
 
@@ -361,14 +361,14 @@ public class ContractListInfo {
 	/**
 	 * サービス利用希望日
 	 */
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
 	@ApiModelProperty(value = "サービス利用希望日", required = false, position = 55)
 	private Date conclusionPreferredDate;
 
 	/**
 	 * 解約予定日
 	 */
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
 	@ApiModelProperty(value = "解約予定日", required = false, position = 56)
 	private Date cancelScheduledDate;
 
