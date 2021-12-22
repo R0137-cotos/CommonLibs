6010e3b49da049fe09127f546886cc4490fe14c6
takumi-sato <Takumi.Sato1@jp.ricoh.com>
2021-12-22 14:43:28 +0900

    O365拡張項目のDTOを新規作成

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/json/estimation/ProductEstimationExtendsParameterO365Dto.java
@@ -0,0 +1,27 @@
+package jp.co.ricoh.cotos.commonlib.dto.json.estimation;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+/**
+ * 商品（見積用）拡張項目DTO（O365）
+ */
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductEstimationExtendsParameterO365Dto {
+
+	/**
+	 * 希望サブドメイン名
+	 */
+	private String desiredSubDomainName;
+
+	/**
+	 * CPQ商品固有戻り値
+	 */
+	@JsonProperty("cpqProductSpecificReturnValue")
+	private CpqReturnValueO365Dto cpqReturnValueO365Dto;
+
+}
