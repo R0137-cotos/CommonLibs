8ba2ad5f1882e8ed1c9dd1e0a97f77bef07dd971
Murayama Tatsuma (村山 竜馬) <tatsuma.murayama@jp.ricoh.com>
2022-02-03 15:28:48 +0900

    LicenseAccountListInfoの項目を削除してしまった為修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/LicenseAccountListInfo.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/LicenseAccountListInfo.java
@@ -32,38 +32,44 @@ public class LicenseAccountListInfo {
 	private String picName;
 
 	/**
+	 * アカウント担当者メールアドレス
+	 */
+	@ApiModelProperty(value = "アカウント担当者メールアドレス", required = false, position = 4, allowableValues = "range[0,255]")
+	private String accountPicMailAddress;
+
+	/**
 	 * アカウント企業名
 	 */
-	@ApiModelProperty(value = "アカウント企業名", required = false, position = 4, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "アカウント企業名", required = false, position = 5, allowableValues = "range[0,255]")
 	private String accountCompanyName;
 
 	/**
 	 * 都道府県
 	 */
-	@ApiModelProperty(value = "都道府県", required = false, position = 5, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "都道府県", required = false, position = 6, allowableValues = "range[0,255]")
 	private String prefectures;
 
 	/**
 	 * インストールURL
 	 */
-	@ApiModelProperty(value = "インストールURL", required = false, position = 6, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "インストールURL", required = false, position = 7, allowableValues = "range[0,255]")
 	private String installUrl;
 
 	/**
 	 * 費用種別
 	 */
-	@ApiModelProperty(value = "費用種別", required = false, position = 7, allowableValues = "初期費(\"1\"), 月額_定額(\"2\"), 年額(\"3\"), 月額_従量(\"4\"), 違約金(\"5\")")
+	@ApiModelProperty(value = "費用種別", required = false, position = 8, allowableValues = "初期費(\"1\"), 月額_定額(\"2\"), 年額(\"3\"), 月額_従量(\"4\"), 違約金(\"5\")")
 	private String costType;
 
 	/**
 	 * 数量
 	 */
-	@ApiModelProperty(value = "数量", required = false, position = 8, allowableValues = "range[-99999,99999]")
+	@ApiModelProperty(value = "数量", required = false, position = 9, allowableValues = "range[-99999,99999]")
 	private Integer licenseQuantity;
 
 	/**
 	 * 商品名
 	 */
-	@ApiModelProperty(value = "商品名", required = false, position = 9, allowableValues = "range[0,255]")
+	@ApiModelProperty(value = "商品名", required = false, position = 10, allowableValues = "range[0,255]")
 	private String productName;
 }
