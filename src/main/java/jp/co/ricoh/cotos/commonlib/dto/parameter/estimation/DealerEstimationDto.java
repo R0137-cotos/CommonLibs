7c793f1ada2d11936de0cd26007bc22749e8fec3
Furukawa Takashi W (古川 宇宙) <takashi.furukawa.W@jp.ricoh.com>
2022-02-04 11:20:16 +0900

    #13376_更新案内メール送信フラグを販売店に追加_release

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/estimation/DealerEstimationDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/estimation/DealerEstimationDto.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.commonlib.dto.parameter.estimation;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModelProperty;
@@ -17,4 +19,12 @@ public class DealerEstimationDto extends DealerAbstractDto {
 	@Size(max = 255)
 	@ApiModelProperty(value = "担当者メールアドレス", required = false, position = 3, allowableValues = "range[0,255]")
 	private String picMailAddress;
+
+	/**
+	 * 更新案内メール送信フラグ
+	 */
+	@Max(9)
+	@Min(0)
+	@ApiModelProperty(value = "更新案内メール送信フラグ", required = false, position = 4, allowableValues = "range[0,9]")
+	private Integer sendUpdateMailFlg;
 }
