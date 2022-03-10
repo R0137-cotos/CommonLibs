48445eb24d08f003d74ac11bc3cb4940960eb3b0
matsubara <Yuto.Matsubara@jp.ricoh.com>
2022-03-10 16:16:23 +0900

    承認アラートマスタEntityの新規作成とカラム追加、アラート情報Dto作成

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/contract/ApprovalAlertInfoDto.java
@@ -0,0 +1,13 @@
+package jp.co.ricoh.cotos.commonlib.dto.parameter.contract;
+
+import lombok.Data;
+
+@Data
+public class ApprovalAlertInfoDto {
+
+	/**
+	 * アラートメッセージ
+	 */
+	private String AlertMessage;
+
+}
