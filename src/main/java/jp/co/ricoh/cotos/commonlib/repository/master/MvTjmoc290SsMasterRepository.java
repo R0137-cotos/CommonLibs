cec6b529cb498918d61557beb55077834b0259f6
Nabeta Kyohei (鍋多 恭平) <Kyohei.Nabeta@jp.ricoh.com>
2022-03-17 14:54:57 +0900

    #14345_crudRepositoryに修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MvTjmoc290SsMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MvTjmoc290SsMasterRepository.java
@@ -1,11 +1,11 @@
 package jp.co.ricoh.cotos.commonlib.repository.master;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.master.MvTjmoc290SsMaster;
 
 @Repository
-public interface MvTjmoc290SsMasterRepository extends JpaRepository<MvTjmoc290SsMaster, Long> {
+public interface MvTjmoc290SsMasterRepository extends CrudRepository<MvTjmoc290SsMaster, Long> {
 
 }
