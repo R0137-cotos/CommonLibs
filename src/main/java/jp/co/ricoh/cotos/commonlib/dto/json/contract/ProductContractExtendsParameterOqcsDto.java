a36ad33bc3d32f2d4ef6b7609bf893b59516241c
matsubara <Yuto.Matsubara@jp.ricoh.com>
2022-05-18 14:28:16 +0900

    オンライン資格商品拡張項目用Dtoの作成

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/json/contract/ProductContractExtendsParameterOqcsDto.java
@@ -0,0 +1,67 @@
+package jp.co.ricoh.cotos.commonlib.dto.json.contract;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jp.co.ricoh.cotos.commonlib.dto.json.JsonEnumType.AccountFixFlg;
+import jp.co.ricoh.cotos.commonlib.dto.json.JsonEnumType.ContractTypeDetails;
+import jp.co.ricoh.cotos.commonlib.dto.json.JsonEnumType.NewExistingAccountType;
+import lombok.Data;
+
+/**
+ * 商品（契約用）拡張項目DTO（OQCS）
+ */
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductContractExtendsParameterOqcsDto {
+	
+	/**
+	 * 現地確認日
+	 */
+	private String onsiteConfirmationDate;
+	
+	/**
+	 * 現地確認日：時間
+	 */
+	private String onsiteConfirmationTime;
+
+	/**
+	 * 設置（予定）日
+	 */
+	private String installationScheduleDate;
+	
+	/**
+	 * 設置（予定）日：時間
+	 */
+	private String installationScheduleTime;
+	
+	/**
+	 * オン資 専用ソフト：問合先
+	 */
+	private String onlineQualificationSoftwareContactInfo;
+	
+	/**
+	 * オン資 専用ソフト：電話番号
+	 */
+	private String onlineQualificationSoftwarePhoneNumber;
+	
+	/**
+	 * レセコンソフト：問合先
+	 */
+	private String receiptComputerSoftwareContactInfo;
+	
+	/**
+	 * レセコンソフト：電話番号
+	 */
+	private String receiptComputerSoftwarePhoneNumber;
+	
+	/**
+	 * 顔認証端末：問合先
+	 */
+	private String faceRecognitionTerminalContactInfo;
+	
+	/**
+	 * 顔認証端末：電話番号
+	 */
+	private String faceRecognitionTerminalPhoneNumber;
+}
