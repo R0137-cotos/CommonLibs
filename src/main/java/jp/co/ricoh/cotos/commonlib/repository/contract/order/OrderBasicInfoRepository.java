5e141cf3e4c1d51385a984f1338f1d34c8d692fe
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2022-06-03 09:29:04 +0900

    [o365]注文登録APIレビュー指摘対応

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/contract/order/OrderBasicInfoRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/contract/order/OrderBasicInfoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.contract.order.OrderBasicInfo;
-
 @Repository
 public interface OrderBasicInfoRepository extends JpaRepository<OrderBasicInfo, Long> {
 
