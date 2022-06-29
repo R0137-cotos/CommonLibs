0afed38c2dca6716ebef47c4c0b8ff3346d13e78
abe <Yoshiki.Abe1@jp.ricoh.com>
2022-06-29 10:12:57 +0900

    14627 OSO申込データ.再連携フラグ追加

--- src/test/java/jp/co/ricoh/cotos/commonlib/check/TestAccounting.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/check/TestAccounting.java
@@ -509,6 +509,7 @@ public class TestAccounting {
 		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "販売元仕入単価は小数点以下2桁を超えています。"));
 	}
 
+	@SuppressWarnings("deprecation")
 	@Test
 	public void OsoRequestDataのテスト() throws Exception {
 
@@ -571,16 +572,18 @@ public class TestAccounting {
 		// 異常系（@Max ：）
 		BeanUtils.copyProperties(testTarget, entity);
 		testTarget.setRtsManageBranchNumber(INT_1000);
+		testTarget.setRecoordinateFlg(INT_1000);
 		result = testSecurityController.callParameterCheck(testTarget, headersProperties, localServerPort);
-		Assert.assertTrue(result.getErrorInfoList().size() == 1);
+		Assert.assertTrue(result.getErrorInfoList().size() == 2);
 		Assert.assertTrue(testTool.errorIdMatchesAll(result.getErrorInfoList(), ParameterErrorIds.ROT00015));
 		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "RTS管理番号枝番は最大値（999）を超えています。"));
 
 		// 異常系（@Min ：）
 		BeanUtils.copyProperties(testTarget, entity);
 		testTarget.setRtsManageBranchNumber(INT_MINUS_1);
+		testTarget.setRecoordinateFlg(INT_MINUS_1);
 		result = testSecurityController.callParameterCheck(testTarget, headersProperties, localServerPort);
-		Assert.assertTrue(result.getErrorInfoList().size() == 1);
+		Assert.assertTrue(result.getErrorInfoList().size() == 2);
 		Assert.assertTrue(testTool.errorIdMatchesAll(result.getErrorInfoList(), ParameterErrorIds.ROT00027));
 		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "RTS管理番号枝番は最小値（0）を下回っています。"));
 	}
