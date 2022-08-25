62375ef5a31f5227a85fbc68ab33fc0e140c7e13
Kanako Michino W (道野 可南子) <michino.kanako.W@jp.ricoh.com>
2022-08-25 19:45:16 +0900

    #16081_指摘事項の対応

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/ProductGrpMaster.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/master/ProductGrpMaster.java
@@ -1,6 +1,7 @@
 
 package jp.co.ricoh.cotos.commonlib.entity.master;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -20,7 +21,11 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
+import org.springframework.context.annotation.Description;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 import io.swagger.annotations.ApiModelProperty;
 import jp.co.ricoh.cotos.commonlib.entity.EntityBaseMaster;
@@ -37,6 +42,29 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 @Table(name = "product_grp_master")
 public class ProductGrpMaster extends EntityBaseMaster {
+
+	@Description(value = "S&S作業依頼ルート区分")
+	public enum SsWorkRequestRootDiv {
+
+		CSV作成("1"), API連携("2");
+
+		private final String text;
+
+		private SsWorkRequestRootDiv(final String text) {
+			this.text = text;
+		}
+
+		@Override
+		@JsonValue
+		public String toString() {
+			return this.text;
+		}
+
+		@JsonCreator
+		public static SsWorkRequestRootDiv fromString(String string) {
+			return Arrays.stream(values()).filter(v -> v.text.equals(string)).findFirst().orElseThrow(() -> new IllegalArgumentException(String.valueOf(string)));
+		}
+	}
 
 	/**
 	 * 商品グループマスタID
@@ -192,7 +220,6 @@ public class ProductGrpMaster extends EntityBaseMaster {
 	/**
 	 * S&S作業依頼ルート区分
 	 */
-	@Size(max = 255)
-	@ApiModelProperty(value = "S&S作業依頼ルート区分", required = false, position = 22, allowableValues = "range[0,255]")
-	private String ssWorkRequestRootDiv;
+	@ApiModelProperty(value = "S&S作業依頼ルート区分", required = false, position = 22, allowableValues = "CSV作成(\"1\"), API連携(\"2\")")
+	private SsWorkRequestRootDiv ssWorkRequestRootDiv;
 }
