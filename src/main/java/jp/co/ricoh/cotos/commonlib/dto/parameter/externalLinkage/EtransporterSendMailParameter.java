e1283102f07b78f5fae57dddbc3a3cc1834dddc5
Koiwa Yoshiki (小岩 祥希) <Yoshiki.Koiwa@jp.ricoh.com>
2022-09-06 16:08:50 +0900

    16218 メール送信APIパラメータ修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/externalLinkage/EtransporterSendMailParameter.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/externalLinkage/EtransporterSendMailParameter.java
@@ -23,37 +23,10 @@ public class EtransporterSendMailParameter {
 	@ApiModelProperty(value = "メールメッセージ", required = false, position = 4, allowableValues = "range[0,20000]")
 	private String mailMessage;
 
-	@ApiModelProperty(value = "メール送信有無", required = true, position = 5)
-	private boolean sendMail;
-
-	@ApiModelProperty(value = "ダウンロード公開期限", required = true, position = 6, allowableValues = "range[-1,99]")
-	private int days;
-
-	@ApiModelProperty(value = "ダウンロード回数制限", required = true, position = 7, allowableValues = "range[-1,99]")
-	private int count;
-
-	@ApiModelProperty(value = "ファイルパスワード設定", required = false, position = 8, allowableValues = "true:ファイルをZIP化する,false:ZIP化しない")
-	private boolean isEnabledFilePassword;
-
-	@ApiModelProperty(value = "ファイルパスワード自動生成設定", required = false, position = 9)
-	private boolean isAutoFilePassword;
-
-	@ApiModelProperty(value = "ファイルパスワード", required = false, position = 10, allowableValues = "range[0,40]")
-	private String filePassword;
-
-	@ApiModelProperty(value = "ダウンロードパスワード設定", required = false, position = 11)
-	private boolean isEnabledDownloadPassword;
-
-	@ApiModelProperty(value = "ダウンロードパスワード自動生成設定", required = false, position = 12)
-	private boolean isAutoDownloadPassword;
-
-	@ApiModelProperty(value = "ダウンロードパスワード", required = false, position = 13, allowableValues = "range[0,40]")
-	private String downloadPassword;
-
-	@ApiModelProperty(value = "アップロードファイル", required = true, position = 14)
+	@ApiModelProperty(value = "アップロードファイル", required = true, position = 5)
 	private List<UploadFileParameter> fileList;
 
-	@ApiModelProperty(value = "API呼び出しリトライ有無", required = false, position = 15)
+	@ApiModelProperty(value = "API呼び出しリトライ有無", required = false, position = 6)
 	private boolean retry;
 
 }
