5b035ec7edef59015a6c615e60e5d6cb051d1f2e
Yokokouji Tetsuhisa (横小路 哲寿) <Tetsuhisa.Yokokouji@jp.ricoh.com>
2022-10-11 13:18:15 +0900

    #16627・#16629_IIJ機器情報テーブルの検索・更新に使用するDtoを作成。

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/externalLinkage/IijMachineInfoDto.java
@@ -0,0 +1,81 @@
+package jp.co.ricoh.cotos.commonlib.dto.parameter.externalLinkage;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModelProperty;
+import jp.co.ricoh.cotos.commonlib.entity.externallinkage.IijMachineInfo.SettingStatus;
+import lombok.Data;
+
+/**
+ * IIJ機器情報を設定するためのキー項目クラスを表します。
+ */
+
+@Data
+public class IijMachineInfoDto {
+
+	/**
+	 * IIJ機器情報ID
+	 */
+	@Min(0)
+	@ApiModelProperty(value = "IIJ機器情報ID(作成時不要)", required = true, position = 1, allowableValues = "range[0,9223372036854775807]", readOnly = true)
+	private long id;
+
+	/**
+	 * シリアル番号
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "シリアル番号", required = false, position = 2, allowableValues = "range[0,255]")
+	private String serialNumber;
+
+	/**
+	 * ディストリビューションID
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "ディストリビューションID", required = false, position = 3, allowableValues = "range[0,255]")
+	private String distributionId;
+
+	/**
+	 * お問い合わせ番号
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "お問い合わせ番号", required = false, position = 4, allowableValues = "range[0,255]")
+	private String contactNo;
+
+	/**
+	 * RJ管理番号
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "RJ管理番号", required = false, position = 5, allowableValues = "range[0,255]")
+	private String rjManageNumber;
+
+	/**
+	 * 契約番号
+	 */
+	@Size(max = 255)
+	@ApiModelProperty(value = "契約番号", required = false, position = 6, allowableValues = "range[0,255]")
+	private String contractNumber;
+
+	/**
+	 * 契約番号枝番
+	 */
+	@Max(99)
+	@Min(0)
+	@ApiModelProperty(value = "契約番号枝番", required = true, position = 7, allowableValues = "range[0,99]")
+	private int contractBranchNumber;
+
+	/**
+	 * 契約ID
+	 */
+	@Min(0)
+	@ApiModelProperty(value = "契約ID", required = true, position = 8, allowableValues = "range[0,9223372036854775807]")
+	private long contractId;
+
+	/**
+	 * 設定状態
+	 */
+	@ApiModelProperty(value = "設定状態", required = false, position = 9, allowableValues = "未設定(\"0\"), 設定済(\"1\"), 解除済(\"2\")")
+	private SettingStatus settingStatus;
+
+}
