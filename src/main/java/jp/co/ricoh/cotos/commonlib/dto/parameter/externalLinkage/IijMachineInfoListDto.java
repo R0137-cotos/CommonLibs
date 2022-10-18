05858d41b76bec37a62527aaf08abf80fe878b40
Yokokouji Tetsuhisa (横小路 哲寿) <Tetsuhisa.Yokokouji@jp.ricoh.com>
2022-10-18 10:48:42 +0900

    #16629 _IIJ機器情報更新に使用するDtoを作成。

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/externalLinkage/IijMachineInfoListDto.java
@@ -0,0 +1,23 @@
+package jp.co.ricoh.cotos.commonlib.dto.parameter.externalLinkage;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * IIJ機器情報リストを設定するためのキー項目クラスを表します。
+ */
+
+@Data
+public class IijMachineInfoListDto {
+
+	/**
+	 * IIJ機器情報リスト
+	 */
+	@ApiModelProperty(value = "IIJ機器情報リスト", required = false, position = 1)
+	@Valid
+	private List<IijMachineInfoDto> iijMachineInfoList;
+}
