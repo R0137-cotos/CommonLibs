4a5d419e7937e4a06e9453bc314694138a7b392d
Yokokouji Tetsuhisa (横小路 哲寿) <Tetsuhisa.Yokokouji@jp.ricoh.com>
2022-11-22 17:11:41 +0900

    #17095_LaIT連携情報テーブルのエンティティ等を追加。

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/converter/externallinkage/InfoDivConverter.java
@@ -0,0 +1,24 @@
+package jp.co.ricoh.cotos.commonlib.converter.externallinkage;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import jp.co.ricoh.cotos.commonlib.entity.externallinkage.LaitLinkageInfo.InfoDiv;
+
+@Converter(autoApply = true)
+public class InfoDivConverter implements AttributeConverter<InfoDiv, String> {
+
+	@Override
+	public String convertToDatabaseColumn(InfoDiv InfoDiv) {
+		if (InfoDiv == null)
+			return null;
+		return InfoDiv.toString();
+	}
+
+	@Override
+	public InfoDiv convertToEntityAttribute(String value) {
+		if (value == null)
+			return null;
+		return InfoDiv.fromString(value); // IllegalArgumentExceptionはfromString側で投げている
+	}
+}
---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/externallinkage/LaitLinkageInfoRepository.java
@@ -0,0 +1,16 @@
+package jp.co.ricoh.cotos.commonlib.repository.externallinkage;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.ricoh.cotos.commonlib.entity.externallinkage.LaitLinkageInfo;
+
+@Repository
+public interface LaitLinkageInfoRepository extends CrudRepository<LaitLinkageInfo, Long> {
+
+	public List<LaitLinkageInfo> findBySaLabel(String saLabel);
+
+	public List<LaitLinkageInfo> findBySaCode(String saCode);
+}
