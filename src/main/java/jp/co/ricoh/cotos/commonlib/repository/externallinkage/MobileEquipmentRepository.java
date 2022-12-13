744ed20719317ab6a782ef14d558f252db2ebfc7
Yokokouji Tetsuhisa (横小路 哲寿) <Tetsuhisa.Yokokouji@jp.ricoh.com>
2022-12-13 15:17:08 +0900

    #17335_モバイル機器のリポジトリメソッド修正。

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/externallinkage/MobileEquipmentRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/externallinkage/MobileEquipmentRepository.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.commonlib.repository.externallinkage;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import jp.co.ricoh.cotos.commonlib.entity.externallinkage.MobileEquipment;
 @Repository
 public interface MobileEquipmentRepository extends CrudRepository<MobileEquipment, Long> {
 
+	public List<MobileEquipment> findByContractId(long contractId);
 }
