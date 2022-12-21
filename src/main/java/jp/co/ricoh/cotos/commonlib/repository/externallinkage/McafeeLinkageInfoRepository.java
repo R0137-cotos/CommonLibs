f90dfdc623b1b39d67129af828759785a26b9d7d
Yokokouji Tetsuhisa (横小路 哲寿) <Tetsuhisa.Yokokouji@jp.ricoh.com>
2022-12-21 11:17:43 +0900

    #17393_McAfee連携情報のリポジトリを追加。

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/externallinkage/McafeeLinkageInfoRepository.java
@@ -0,0 +1,11 @@
+package jp.co.ricoh.cotos.commonlib.repository.externallinkage;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.ricoh.cotos.commonlib.entity.externallinkage.McafeeLinkageInfo;
+
+@Repository
+public interface McafeeLinkageInfoRepository extends CrudRepository<McafeeLinkageInfo, Long> {
+
+}
