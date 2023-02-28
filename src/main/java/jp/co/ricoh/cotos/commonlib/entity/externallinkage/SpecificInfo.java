4cb853ed3a9fc746f4aebef265ae68316b21086e
Nabeta Kyohei (鍋多 恭平) <Kyohei.Nabeta@jp.ricoh.com>
2023-02-28 10:44:34 +0900

    #17964_商材固有項目エンティティの不要なアノテーションを削除

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/externallinkage/SpecificInfo.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/externallinkage/SpecificInfo.java
@@ -69,7 +69,6 @@ public class SpecificInfo extends EntityBase {
 	 * 項目値区分
 	 */
 	@Column(nullable = false)
-	@Size(max = 255)
 	@ApiModelProperty(value = "項目値区分", required = true, position = 6, allowableValues = "range[0,255]")
 	private ItemValueDiv itemValueDiv;
 
