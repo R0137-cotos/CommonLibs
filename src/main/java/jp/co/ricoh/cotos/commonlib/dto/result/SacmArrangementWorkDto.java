6d15b7f367c1bb4b4cf4d9948416ee539df3d3ce
RuneKurokawa <Rune.Kurokawa@jp.ricoh.com>
2023-03-07 13:30:01 +0900

    #18039 Swaggerアノテーション追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/SacmArrangementWorkDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/SacmArrangementWorkDto.java
@@ -2,6 +2,7 @@ package jp.co.ricoh.cotos.commonlib.dto.result;
 
 import java.util.List;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -14,31 +15,37 @@ public class SacmArrangementWorkDto {
 	/**
 	 * 手配業務ID
 	 */
+	@ApiModelProperty(value = "手配業務ID", required = false, position = 1)
 	private String arrangementWorkId;
 
 	/**
 	 * 文書番号
 	 */
+	@ApiModelProperty(value = "文書番号", required = false, position = 2)
 	private String estimationNumber;
 
 	/**
 	 * 契約ID
 	 */
+	@ApiModelProperty(value = "契約ID", required = false, position = 3)
 	private String rjManageNumber;
 
 	/**
 	 * CSV取込日
 	 */
+	@ApiModelProperty(value = "CSV取込日", required = false, position = 4)
 	private String csvImportDate;
 
 	/**
 	 * コンフィグファイル名
 	 */
+	@ApiModelProperty(value = "コンフィグファイル名", required = false, position = 5)
 	private String configFileName;
 
 	/**
 	 * LaITコンフィグデータリスト
 	 */
+	@ApiModelProperty(value = "LaITコンフィグデータリスト", required = false, position = 6)
 	private List<LaitConfigDataDto> laitConfigDataList;
 
 }
--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/SacmPresetInfoDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/SacmPresetInfoDto.java
@@ -2,6 +2,7 @@ package jp.co.ricoh.cotos.commonlib.dto.result;
 
 import java.util.List;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -14,6 +15,7 @@ public class SacmPresetInfoDto {
 	/**
 	 * 手配業務リスト
 	 */
+	@ApiModelProperty(value = "手配業務リスト", required = false, position = 1)
 	private List<SacmArrangementWorkDto> arrangementWorkList;
 
 }
