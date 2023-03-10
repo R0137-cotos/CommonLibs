7deb423d8c9a03f04558a6f4556a2da54fb6a6be
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2023-03-10 17:17:07 +0900

    [O365]アップグレード対応 エンティティ作成

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/LicenseUpgradeItemMasterRepository.java
@@ -0,0 +1,9 @@
+package jp.co.ricoh.cotos.commonlib.repository.master;
+
+import org.springframework.data.repository.CrudRepository;
+
+import jp.co.ricoh.cotos.commonlib.entity.master.LicenseUpgradeItemMaster;
+
+public interface LicenseUpgradeItemMasterRepository extends CrudRepository<LicenseUpgradeItemMaster, Long> {
+
+}
