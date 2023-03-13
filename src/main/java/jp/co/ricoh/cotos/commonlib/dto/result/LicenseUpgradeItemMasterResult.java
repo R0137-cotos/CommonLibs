5ed4692c364450d9ab43cc83eaa251a35c6e5d07
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2023-03-13 09:36:51 +0900

    [O365]アップグレード対応 swagger追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/LicenseUpgradeItemMasterResult.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/LicenseUpgradeItemMasterResult.java
@@ -3,6 +3,7 @@ package jp.co.ricoh.cotos.commonlib.dto.result;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -16,40 +17,48 @@ public class LicenseUpgradeItemMasterResult {
 	 * アップグレード対象ライセンス対応品種マスタID
 	 */
 	@Id
+	@ApiModelProperty(value = "アップグレード対象ライセンス対応品種マスタID(作成時不要)", required = true, position = 1)
 	private Long id;
 
 	/**
 	 * 商品マスタID
 	 */
+	@ApiModelProperty(value = "商品マスタID", required = true, position = 2)
 	private Long productMasterId;
 
 	/**
 	 * アップグレード元品種マスタID
 	 */
+	@ApiModelProperty(value = "アップグレード元品種マスタID", required = true, position = 3)
 	private Long fromItemMasterId;
 
 	/**
 	 * アップグレード元品種コード
 	 */
+	@ApiModelProperty(value = "アップグレード元品種コード", required = true, position = 4)
 	private String fromRicohItemCode;
 
 	/**
 	 * アップグレード元品種名
 	 */
+	@ApiModelProperty(value = "アップグレード元品種名", required = true, position = 5)
 	private String fromItemName;
 
 	/**
 	 * アップグレード先品種マスタID
 	 */
+	@ApiModelProperty(value = "アップグレード先品種マスタID", required = true, position = 6)
 	private Long toItemMasterId;
 
 	/**
 	 * アップグレード先品種コード
 	 */
+	@ApiModelProperty(value = "アップグレード先品種コード", required = true, position = 7)
 	private String toRicohItemCode;
 
 	/**
 	 * アップグレード先品種名
 	 */
+	@ApiModelProperty(value = "アップグレード先品種名", required = true, position = 8)
 	private String toItemName;
 }
