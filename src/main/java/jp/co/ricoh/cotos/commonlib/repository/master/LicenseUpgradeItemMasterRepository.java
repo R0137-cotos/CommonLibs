8bfcfc9038e6035ff7993066a32384b227e78b06
Katsuharu Sasayama1 <Katsuharu.Sasayama1@jp.ricoh.com>
2023-03-22 08:20:40 +0900

    アップグレード対象ライセンス品種対応マスタを商品マスタIDごとに取得できるようにする

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/LicenseUpgradeItemMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/LicenseUpgradeItemMasterRepository.java
@@ -1,9 +1,13 @@
 package jp.co.ricoh.cotos.commonlib.repository.master;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import jp.co.ricoh.cotos.commonlib.entity.master.LicenseUpgradeItemMaster;
 
 public interface LicenseUpgradeItemMasterRepository extends CrudRepository<LicenseUpgradeItemMaster, Long> {
+
+	public List<LicenseUpgradeItemMaster> findByProductMasterId(long productMasterId);
 
 }
