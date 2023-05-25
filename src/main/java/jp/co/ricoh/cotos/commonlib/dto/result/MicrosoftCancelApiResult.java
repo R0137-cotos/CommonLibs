7ce0eb9adad7945da83f327123977f7c802761e6
yoshikatu.shimomura@jp.ricoh.com <yoshikatu.shimomura@jp.ricoh.com>
2023-05-25 10:54:53 +0900

    #19112　MSキャンセルAPI

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/result/MicrosoftCancelApiResult.java
@@ -0,0 +1,24 @@
+package jp.co.ricoh.cotos.commonlib.dto.result;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * MicrosoftキャンセルAPIの戻り値を返却するためのDtoです。<br/>
+ * このクラスを使用してDBへの保存を行うことは出来ません。
+ */
+@Data
+public class MicrosoftCancelApiResult {
+
+	/**
+	 * コード
+	 */
+	@ApiModelProperty(value = "コード", required = false, position = 1, allowableValues = "range[0,255]")
+	private String code;
+
+	/**
+	 * メッセージ
+	 */
+	@ApiModelProperty(value = "メッセージ", required = false, position = 2, allowableValues = "range[0,255]")
+	private String apiMessage;
+}
