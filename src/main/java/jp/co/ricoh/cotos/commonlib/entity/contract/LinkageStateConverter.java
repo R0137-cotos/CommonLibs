30da4f4bffec881a771679db2f302a97451287bc
RuneKurokawa <Rune.Kurokawa@jp.ricoh.com>
2023-06-15 08:29:08 +0900

    #19407 回収先.連携状態のConverter追加

---
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/contract/LinkageStateConverter.java
@@ -0,0 +1,25 @@
+package jp.co.ricoh.cotos.commonlib.entity.contract;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import jp.co.ricoh.cotos.commonlib.entity.contract.CollectLocation.LinkageState;
+
+
+@Converter(autoApply = true)
+public class LinkageStateConverter implements AttributeConverter<LinkageState, String> {
+
+	@Override
+	public String convertToDatabaseColumn(LinkageState linkageState) {
+		if (linkageState == null)
+			return null;
+		return linkageState.toString();
+	}
+
+	@Override
+	public LinkageState convertToEntityAttribute(String value) {
+		if (value == null)
+			return null;
+		return LinkageState.fromString(value); // IllegalArgumentExceptionはfromString側で投げている
+	}
+}
