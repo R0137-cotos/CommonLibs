f36a82f578c1be8ec50781f00ecfd083997bf061
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2023-07-26 09:45:59 +0900

    レビュー指摘事項対応

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/license/ms/MsSubscriptionRegisterRequestWork.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/license/ms/MsSubscriptionRegisterRequestWork.java
@@ -36,7 +36,7 @@ public class MsSubscriptionRegisterRequestWork extends AbstractMsRequestWork {
 	@Description(value = "増減区分")
 	public enum IncreaseDecreaseDiv {
 
-		増数("1"), 減数("2"), アップグレード("3"), 契約期間の更新("4");
+		増数("1"), 減数("2"), アップグレード("3"), 契約期間の変更("4");
 
 		private final String text;
 
