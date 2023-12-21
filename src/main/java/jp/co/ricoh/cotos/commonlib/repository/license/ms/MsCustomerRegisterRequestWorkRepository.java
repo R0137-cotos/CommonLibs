d8257f04d179ab126417ee74b4f08d52e54c72ab
Ono Kosei (小野 航征) <kosei.ono@jp.ricoh.com>
2023-12-21 11:42:25 +0900

    コンフリクト修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/license/ms/MsCustomerRegisterRequestWorkRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/license/ms/MsCustomerRegisterRequestWorkRepository.java
@@ -1,14 +1,19 @@
 package jp.co.ricoh.cotos.commonlib.repository.license.ms;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.license.ms.MsCustomerRegisterRequestWork;
+import jp.co.ricoh.cotos.commonlib.entity.license.ms.MsCustomerRegisterRequestWork.MsCustomerLinkageStatus;
 
 @Repository
 public interface MsCustomerRegisterRequestWorkRepository extends CrudRepository<MsCustomerRegisterRequestWork, Long> {
 	public MsCustomerRegisterRequestWork findByContractId(long contractId);
 
 	public MsCustomerRegisterRequestWork findByLicenseInfoId(long licenseInfoId);
+
+	public List<MsCustomerRegisterRequestWork> findByCustomerLinkageStatus(MsCustomerLinkageStatus customerLinkageStatus);
 
 }
