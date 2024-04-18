689f2a0899b478d412a644b00dcb9cf21c2bb839
Kanako Michino W (道野 可南子) <michino.kanako.W@jp.ricoh.com>
2024-04-18 11:23:11 +0900

    enum定義を削除

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/json/license/LicenseDetailExtendsParameterMitsDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/json/license/LicenseDetailExtendsParameterMitsDto.java
@@ -1,11 +1,8 @@
 package jp.co.ricoh.cotos.commonlib.dto.json.license;
 
-import java.util.Arrays;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonValue;
 
+import jp.co.ricoh.cotos.commonlib.dto.json.license.LicenseDetailExtendsParameterGspDto.ReceptionStatusFlg;
 import lombok.Data;
 
 /**
@@ -14,28 +11,6 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LicenseDetailExtendsParameterMitsDto {
-
-	public enum ReceptionStatusFlg {
-
-		未("0"), 受理("1"), 不受理("2");
-
-		private final String text;
-
-		private ReceptionStatusFlg(final String text) {
-			this.text = text;
-		}
-
-		@Override
-		@JsonValue
-		public String toString() {
-			return this.text;
-		}
-
-		@JsonCreator
-		public static ReceptionStatusFlg fromString(String string) {
-			return Arrays.stream(values()).filter(v -> v.text.equals(string)).findFirst().orElseThrow(() -> new IllegalArgumentException(String.valueOf(string)));
-		}
-	}
 
 	/**
 	 * 受付状況フラグ
