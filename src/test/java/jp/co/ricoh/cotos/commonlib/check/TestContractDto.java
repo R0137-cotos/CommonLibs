626c23ba87edf4411f2d8ab57e58f4357804f700
Tsu Koushou (津 孔祥) <Koushou.Tsu@jp.ricoh.com>
2024-04-22 17:19:58 +0900

    #23946_品種明細（契約用）Dtoに項目追加

--- src/test/java/jp/co/ricoh/cotos/commonlib/check/TestContractDto.java
+++ src/test/java/jp/co/ricoh/cotos/commonlib/check/TestContractDto.java
@@ -697,6 +697,22 @@ public class TestContractDto {
 		Assert.assertTrue(testTool.errorIdMatchesAll(result.getErrorInfoList(), ParameterErrorIds.ROT00014));
 		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "振替先課所コードは最大文字数（255）を超えています。"));
 
+		// 異常系（@Max ：）
+		BeanUtils.copyProperties(testTarget, entity);
+		testTarget.setBatchImportFlg(INT_10);
+		result = testSecurityController.callParameterCheck(testTarget, headersProperties, localServerPort);
+		Assert.assertTrue(result.getErrorInfoList().size() == 1);
+		Assert.assertTrue(testTool.errorIdMatchesAll(result.getErrorInfoList(), ParameterErrorIds.ROT00015));
+		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "一括登録フラグは最大値（9）を超えています。"));
+
+		// 異常系（@Min ：）
+		BeanUtils.copyProperties(testTarget, entity);
+		testTarget.setBatchImportFlg(INT_MINUS_1);
+		result = testSecurityController.callParameterCheck(testTarget, headersProperties, localServerPort);
+		Assert.assertTrue(result.getErrorInfoList().size() == 1);
+		Assert.assertTrue(testTool.errorIdMatchesAll(result.getErrorInfoList(), ParameterErrorIds.ROT00027));
+		Assert.assertTrue(testTool.errorMessageMatchesOne(result.getErrorInfoList(), "一括登録フラグは最小値（0）を下回っています。"));
+
 		// 異常系（@DecimalMin ：）
 		BeanUtils.copyProperties(entity, testTarget);
 		testTarget.setPrice(DECIMAL_MINUS_1000000000000000000001);
