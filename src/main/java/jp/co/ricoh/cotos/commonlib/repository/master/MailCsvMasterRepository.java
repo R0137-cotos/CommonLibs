4290653bfb9a35c289df0e352b37fb5b122009ee
RFG\r23000014 <Naoya.Cho@jp.ricoh.com>
2024-06-25 11:48:24 +0900

    自動テスト設定、バージョンについて追記

--- src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MailCsvMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/repository/master/MailCsvMasterRepository.java
@@ -1,11 +1,11 @@
 package jp.co.ricoh.cotos.commonlib.repository.master;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.commonlib.entity.master.MailCsvMaster;
 
 @Repository
-public interface MailCsvMasterRepository extends JpaRepository<MailCsvMaster, Long> {
+public interface MailCsvMasterRepository extends CrudRepository<MailCsvMaster, Long> {
 
 }
