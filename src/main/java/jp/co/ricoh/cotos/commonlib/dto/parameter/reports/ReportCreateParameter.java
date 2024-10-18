0a2136e54f9313682d47be0abd4a7e9e79209190
Tsu Koushou (津 孔祥) <Koushou.Tsu@jp.ricoh.com>
2024-10-18 10:47:28 +0900

    #27110_帳票作成パラメーターに項目追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/reports/ReportCreateParameter.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/reports/ReportCreateParameter.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -47,4 +49,12 @@ public class ReportCreateParameter {
 	 */
 	@ApiModelProperty(value = "CreateFormワークディレクトリ", required = false, position = 4)
 	private String workAbsoluteDir;
+
+	/**
+	 * 電子契約フラグ
+	 */
+	@Max(9)
+	@Min(0)
+	@ApiModelProperty(value = "電子契約フラグ", required = false, position = 5)
+	private Integer electronicContractFlg;
 }
