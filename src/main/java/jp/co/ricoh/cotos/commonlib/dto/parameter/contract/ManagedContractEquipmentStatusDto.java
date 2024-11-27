cb083792e352b306dd7beb580ff0712ff3e117a5
takakiTakeda <takaki.takeda@jp.ricoh.com>
2024-11-27 15:06:29 +0900

    契約機種状態管理Dtoのアノテーション修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/contract/ManagedContractEquipmentStatusDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/parameter/contract/ManagedContractEquipmentStatusDto.java
@@ -1,22 +1,10 @@
 package jp.co.ricoh.cotos.commonlib.dto.parameter.contract;
 
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import io.swagger.annotations.ApiModelProperty;
 import jp.co.ricoh.cotos.commonlib.dto.parameter.common.DtoBase;
-import jp.co.ricoh.cotos.commonlib.entity.contract.Contract;
-import jp.co.ricoh.cotos.commonlib.entity.contract.ContractEquipment;
-import jp.co.ricoh.cotos.commonlib.entity.contract.ContractEquipmentItemLink;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -25,21 +13,18 @@ import lombok.EqualsAndHashCode;
 public class ManagedContractEquipmentStatusDto extends DtoBase {
 
 	/**
-	 * 契約
+	 * 契約ID
 	 */
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "contract_id", referencedColumnName = "id")
-	@JsonIgnore
-	@ApiModelProperty(value = "契約", required = true, position = 3)
-	private Contract contract;
+	@Min(0)
+	@ApiModelProperty(value = "契約ID", required = true, position = 3)
+	private long contractId;
 
 	/**
-	 * 契約機種
+	 * 契約機種ID
 	 */
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "contract_equipment_id", referencedColumnName = "id")
-	@ApiModelProperty(value = "契約機種", required = true, position = 4)
-	private ContractEquipment contractEquipment;
+	@Min(0)
+	@ApiModelProperty(value = "契約機種ID", required = true, position = 4)
+	private long contractEquipmentId;
 
 	/**
 	 * 継続フラグ
@@ -73,16 +58,10 @@ public class ManagedContractEquipmentStatusDto extends DtoBase {
 	@ApiModelProperty(value = "契約年数", required = false, position = 8, allowableValues = "range[0,99999]")
 	private Integer contractYears;
 
-	@Valid
-	@OneToMany(mappedBy = "managedContractEquipmentStatus")
-	@ApiModelProperty(value = "契約機種品種紐づけシーケンス", required = false, position = 9)
-	private List<ContractEquipmentItemLink> contractEquipmentItemLinkList;
-
 	/**
 	 * 機器削除契約ID
 	 */
 	@Min(0)
-	@Column(nullable = false)
-	@ApiModelProperty(value = "機器削除契約ID", required = false, position = 10, allowableValues = "range[0,9223372036854775807]")
+	@ApiModelProperty(value = "機器削除契約ID", required = false, position = 9, allowableValues = "range[0,9223372036854775807]")
 	private long equipmentDeletedContractId;
 }
