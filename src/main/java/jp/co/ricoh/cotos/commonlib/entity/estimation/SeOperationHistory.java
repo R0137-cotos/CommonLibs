77cebfd7af32e8551e85e32f569195074af6127a
Iwasaki Shiho (岩﨑 志穂) <Shiho.Iwasaki@jp.ricoh.com>
2025-02-18 11:45:39 +0900

    OJT課題_エンティティ・リポジトリ作成_フォーマット修正

--- src/main/java/jp/co/ricoh/cotos/commonlib/entity/estimation/SeOperationHistory.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/entity/estimation/SeOperationHistory.java
@@ -42,7 +42,7 @@ public class SeOperationHistory extends EntityBase {
 
 		private final String text;
 
-		private Domain (final String text) {
+		private Domain(final String text) {
 			this.text = text;
 		}
 
@@ -65,7 +65,7 @@ public class SeOperationHistory extends EntityBase {
 
 		private final String text;
 
-		private ProcessingCategory (final String text) {
+		private ProcessingCategory(final String text) {
 			this.text = text;
 		}
 
@@ -132,7 +132,7 @@ public class SeOperationHistory extends EntityBase {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date expirationFrom;
 
-    /**
+	/**
 	 * 有効期限To
 	 */
 	@Column(nullable = false)
