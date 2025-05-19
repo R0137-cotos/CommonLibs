5b3b9b4102dffb8984feb7438f9e7b6be9627021
Nabeta Kyohei (鍋多 恭平) <Kyohei.Nabeta@jp.ricoh.com>
2025-05-19 13:26:53 +0900

    #30631_CSV用のDtoにアノテーションを追加

--- src/main/java/jp/co/ricoh/cotos/commonlib/dto/csv/common/ProductStackingFileDto.java
+++ src/main/java/jp/co/ricoh/cotos/commonlib/dto/csv/common/ProductStackingFileDto.java
@@ -2,7 +2,9 @@ package jp.co.ricoh.cotos.commonlib.dto.csv.common;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import jp.co.ricoh.cotos.commonlib.logic.csv.EmptyToNullDeserializer;
 import lombok.Data;
 
 @Data
@@ -11,42 +13,49 @@ public class ProductStackingFileDto {
 	/**
 	 * 品種コード
 	 */
+	@JsonDeserialize(using = EmptyToNullDeserializer.class)
 	@JsonProperty("品種コード")
 	private String ricohItemCode;
 
 	/**
 	 * 品種名
 	 */
+	@JsonDeserialize(using = EmptyToNullDeserializer.class)
 	@JsonProperty("品種名")
 	private String itemName;
 
 	/**
 	 * 数量
 	 */
+	@JsonDeserialize(using = EmptyToNullDeserializer.class)
 	@JsonProperty("数量")
 	private String quantity;
 
 	/**
 	 * 単価（E/U売価）
 	 */
+	@JsonDeserialize(using = EmptyToNullDeserializer.class)
 	@JsonProperty("単価（E/U売価）")
 	private String unitPrice;
 
 	/**
 	 * システム用項目１（単価（母店仕切））
 	 */
+	@JsonDeserialize(using = EmptyToNullDeserializer.class)
 	@JsonProperty("システム用項目１")
 	private String rjDividingPrice;
 
 	/**
 	 * システム用項目２（単価（接点店仕切））
 	 */
+	@JsonDeserialize(using = EmptyToNullDeserializer.class)
 	@JsonProperty("システム用項目２")
 	private String motherStorePrice;
 
 	/**
 	 * ファイルバージョン
 	 */
+	@JsonDeserialize(using = EmptyToNullDeserializer.class)
 	@JsonProperty("ver")
 	private String importFileVersion;
 }
